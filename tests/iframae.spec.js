f01ba166860fd2af85cb9af755e3567f2456a455
MANASA NARI <abhi99575@gmail.com>
2026-01-26 23:05:07 +0530

    fixed the upload file issue

--- tests/iframae.spec.js
+++ tests/iframae.spec.js
@@ -7,8 +7,8 @@ const Textframe=page.frame({url:'https://demoqa.com/sample'});
 const text=await Textframe.locator("#sampleHeading").textContent();
 console.log(text);
 })
-test('file upload',async({page})=>{
+test.only('file upload',async({page})=>{
     await page.goto('https://the-internet.herokuapp.com/upload');
-    await page.setInputFiles("#file-upload","C:\Users\MAHESH NARI\Downloads\Abhilash_QA_Automation_Playwright_Resume.pdf");
+    await page.setInputFiles("#file-upload","C:/Users/MAHESH NARI/Downloads/Abhilash_QA_Automation_Playwright_Resume.pdf");
     await page.locator('#file-submit').click();
 })
