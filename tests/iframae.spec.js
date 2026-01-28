bbbf1ebf8938ca0f554b546dc946f4552e2c177c
MANASA NARI <abhi99575@gmail.com>
2026-01-28 20:03:00 +0530

    changing pipeline to autotrigger

--- tests/iframae.spec.js
+++ tests/iframae.spec.js
@@ -16,4 +16,11 @@ test('file upload 4',async({page})=>{
     await page.goto('https://the-internet.herokuapp.com/upload');
     await page.setInputFiles("#file-upload","utils/QA Automation Engineer, India 2025 .pdf");
     await page.locator('#file-submit').click();
+    
+})
+test('file upload 5',async({page})=>{
+    await page.goto('https://the-internet.herokuapp.com/upload');
+    await page.setInputFiles("#file-upload","utils/QA Automation Engineer, India 2025 .pdf");
+    await page.locator('#file-submit').click();
+    
 })
